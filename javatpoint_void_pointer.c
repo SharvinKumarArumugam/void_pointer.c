0713afa212a80e6164b6c67f8b4896b8a6040b4b
Mr.Sharvin <93881528+SharvinKumarArumugam@users.noreply.github.com>
2022-03-15 23:51:03 +0800

    Add files via upload

---
+++ javatpoint_void_pointer.c
@@ -0,0 +1,26 @@
+//DOCUMENTATION SECTION 
+/*SHARVIN KUMAR ARUMUGAM
+15/03/2022*/
+
+//PRE-PROCESSOR SECTION 
+#include<stdio.h>
+
+//GLOBAL VARIABLE SECTION
+
+//MAIN FUNCTION SECTION
+int main()
+{
+    void*ptr = NULL;
+    int*p = NULL;
+    char*cp = NULL;
+    float*fp= NULL;
+
+    printf("size of void pointer = %d\n\n",sizeof(ptr));
+    printf("size of void pointer = %d\n\n",sizeof(p));
+    printf("size of void pointer = %d\n\n",sizeof(cp));
+    printf("size of void pointer = %d\n\n",sizeof(fp));
+
+    return 0;
+
+}
+//USER-DEFINED SECTION
